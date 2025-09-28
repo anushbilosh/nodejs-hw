7c2772e43e4f7590758a0441316325eef5939c67
Anush Biloshytska <biloshytska.anush@icloud.com>
2025-09-28 19:57:09 +0300

    node hw1

---
+++ src/server.js
@@ -0,0 +1,65 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import pino from 'pino-http';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Notes Page' });
+});
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+// Маршрут для тестування middleware помилки
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+// Middleware 404 (після всіх маршрутів)
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Middleware для обробки помилок (останнє)
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: 'Simulated server error',
+    error: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
