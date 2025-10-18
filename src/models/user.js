c5ed214f846273bbebb033ef3a43703a58853215
Anush Biloshytska <biloshytska.anush@icloud.com>
2025-10-18 18:33:03 +0300

    hw

---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: false, trim: true },
+    email: { type: String, required: true, trim: true, unique: true },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+userSchema.pre('save', function (next) {
+  if (!this.username) {
+    this.username = this.email;
+  }
+  next();
+});
+
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+export const User = model('User', userSchema);
--- src/server.js
+++ src/server.js
@@ -6,8 +6,11 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 import { logger } from './middleware/logger.js';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 const PORT = process.env.PORT ?? 3030;
 
@@ -15,9 +18,11 @@ const PORT = process.env.PORT ?? 3030;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use(helmet());
 
 // Routes
+app.use(authRoutes);
 app.use(notesRoutes);
 
 // Middleware 404 (після всіх маршрутів)
